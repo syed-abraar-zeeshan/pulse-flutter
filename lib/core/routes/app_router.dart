4b83975e21478a4e75841a4b9d570b09653c6401
Syed Abraar Zeeshan <abraar.zeeshan@vrindasoft.com>
2026-07-16 19:04:58 +0530

    ADDED TEMPORARY SPLASH SCREEN

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,7 +1,18 @@
 import 'package:go_router/go_router.dart';
+import 'package:pulse_flutter/core/routes/route_constants.dart';
+import 'package:pulse_flutter/features/auth/presentation/screens/splash_screen.dart';
 
 class AppRouter {
   AppRouter._();
 
-  static final router = GoRouter(routes: []);
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: RouteConstants.splash,
+        builder: (context, state) {
+          return const SplashScreen();
+        },
+      ),
+    ],
+  );
 }
